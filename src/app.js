e014aa1d17cbafc1efc6695d1e5c01b618857950
Saeed Andalib <saeedandalib5@gmail.com>
2022-06-08 17:45:01 +0430

    finished

--- src/app.js
+++ src/app.js
@@ -1,22 +1,58 @@
+const path = require('path');
 const express = require('express');
-const socket = require('socket.io');
+const http = require('http');
+const {Server} = require('socket.io');
 const WebSocket = require('ws');
 
 // Constants
-const PORT = 3000;
+const PORT = 8080;
 
 // Init
 const app = express();
 const krakenSocket = new WebSocket('wss://ws.kraken.com');
+const server = http.createServer(app);
+const io = new Server(server, {pingInterval: 1000, pingTimeout: 5000});
 
+// Auth middleware: forwards the request is token is 'abcd'
+io.use((socket, next) => {
+  const token = socket.handshake.auth.token;
+
+  if (token !== 'abcd') {
+    const error = new Error('Client Unauthorized');
+    next(error);
+  }
+
+  next()
+})
+
+const sendNewAsk = (ask) => {
+  io.emit('newAsk', ask);
+}
+
+// Ask data from Kraken
 krakenSocket.on('open', () => {
-  krakenSocket.send('{"event":"subscribe","pair":["XBT/USD","XBT/EUR","ADA/USD"],"subscription":{"name":"ticker"}}')
+  krakenSocket.send(JSON.stringify({
+    event: 'subscribe',
+    pair: ["XBT/USD", "XBT/EUR", "ADA/USD"],
+    subscription: {
+      name: 'ticker'
+    }
+  }))
 
   krakenSocket.on('message', (data) => {
-    console.log(JSON.parse(data.toString()))
+    const jsonData = JSON.parse(data);
+
+    // Ignoring the heartbeat
+    if (jsonData.event !== 'heartbeat') {
+      sendNewAsk(jsonData)
+    }
   })
 })
 
-app.listen(PORT, () => {
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'static', 'index.html'))
+})
+
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
